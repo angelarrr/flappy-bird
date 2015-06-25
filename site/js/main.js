87b531d58387c2c97b745032b3b0d971aec29e8e
angelarrr <kwon.angela@gmail.com>
2015-06-25 14:43:55 -0400

    create pipe entity

--- site/js/main.js
+++ site/js/main.js
@@ -2,5 +2,7 @@ var flappyBird = require('./flappy_bird');
 
 document.addEventListener('DOMContentLoaded', function() {
 	var app = new flappyBird.FlappyBird();
+	var pipeApp = new flappyBird.Pipe();
 	app.run();
+	pipeApp.run();
 });
