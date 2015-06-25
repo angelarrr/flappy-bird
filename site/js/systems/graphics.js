4da668c7a7274bd2002d1c67eb2e8f8d7d5b2238
angelarrr <kwon.angela@gmail.com>
2015-06-25 17:12:28 -0400

    update draw method to draw circle

--- site/js/systems/graphics.js
+++ site/js/systems/graphics.js
@@ -1,15 +1,27 @@
 var GraphicsSystem = function(entities) {
 	this.entities = entities;
+	// canvas is where we draw
+	this.canvas = document.getElementById('main-canvas');
+	// context is what we draw to
+	this.context = this.canvas.getContext('2d');
 };
 
 GraphicsSystem.prototype.run = function() {
-	// tick the graphics system a few times to see it in action
-	for (var i=0; i<5; i++) {
-		this.tick();
-	}
+	// run the render loop
+	window.requestAnimationFrame(this.tick.bind(this));
 };
 
 GraphicsSystem.prototype.tick = function() {
+	// set the canvas to the correct size if the window is resized
+	if (this.canvas.width != this.canvas.offsetWidth || this.canvas.height != this.canvas.offsetHeight) {
+		this.canvas.width = this.canvas.offsetWidth;
+		this.canvas.height = this.canvas.offsetHeight;
+	}
+
+	//clear the canvas
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+	// rendering goes here
 	for (var i=0; i<this.entities.length; i++) {
 		var entity = this.entities[i];
 		if (!'graphics' in entity.components) {
@@ -18,6 +30,9 @@ GraphicsSystem.prototype.tick = function() {
 
 		entity.components.graphics.draw(this.context);
 	}
+
+	// continue the render loop
+	window.requestAnimationFrame(this.tick.bind(this));
 };
 
 exports.GraphicsSystem = GraphicsSystem;
