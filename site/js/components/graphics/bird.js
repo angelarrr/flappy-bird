3a21aa1fc2a5cc56d3c6da2ef77e84795abbc936
angelarrr <kwon.angela@gmail.com>
2015-06-30 14:30:34 -0400

    add flappy bird image

--- site/js/components/graphics/bird.js
+++ site/js/components/graphics/bird.js
@@ -7,11 +7,14 @@ BirdGraphicsComponent.prototype.draw = function(context) {
 
 	context.save();
 	context.translate(position.x, position.y);
-	context.beginPath();
-	context.arc(0, 0, 0.02, 0, 2 * Math.PI);
-	context.fillStyle = "yellow";
-	context.fill();
-	context.closePath();
+	var img = new Image();
+	img.src = "img/bird.png";
+	context.drawImage(img, 0, 0, 0.08, 0.08);
+	// context.beginPath();
+	// context.arc(0, 0, 0.02, 0, 2 * Math.PI);
+	// context.fillStyle = "yellow";
+	// context.fill();
+	// context.closePath();
 	context.restore();
 };
 
